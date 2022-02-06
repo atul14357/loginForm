12b1c04c1d46b51bc6bf2360e9e46b5ace8ad9a6
Atul Kaushal <atulkaushal14357@gmail.com>
2022-02-06 22:45:25 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD5j0arCSZrrCqr1NXvhOmofm2FQK4YYqI",
+  authDomain: "signup-d7f03.firebaseapp.com",
+  databaseURL: "https://signup-d7f03-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "signup-d7f03",
+  storageBucket: "signup-d7f03.appspot.com",
+  messagingSenderId: "82380223851",
+  appId: "1:82380223851:web:3f8360775730cd5c46882e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
